4112eb043d985793f317f47b10cc783dd465a5f4
santhoshps0914 <san14kumarps@gmail.com>
2026-06-05 10:13:19 +0530

    ReverseStringUsingRecursion

--- src/stringrecurrsions/ReverseStringUsingRecursion.java
+++ src/stringrecurrsions/ReverseStringUsingRecursion.java
@@ -1,4 +1,15 @@
 package stringrecurrsions;
 
-public class ReverseStringUsingRecurssion {
+public class ReverseStringUsingRecursion {
+
+    public static String reverseStr(String str) {
+        if (str.isEmpty())
+            return str;
+        return reverseStr(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(reverseStr(str));
+    }
 }
